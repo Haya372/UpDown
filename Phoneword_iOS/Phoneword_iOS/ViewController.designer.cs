407e4d9e221ae8cbc48ff628965108cf3e14b37b
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-05-04 23:50:13 +0900

    UIの追加(StoryBoard上に)

--- Phoneword_iOS/Phoneword_iOS/ViewController.designer.cs
+++ Phoneword_iOS/Phoneword_iOS/ViewController.designer.cs
@@ -11,8 +11,37 @@ using UIKit;
 
 namespace Phoneword_iOS
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton CallButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField PhoneNumberText { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton TranslateButton { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (CallButton != null) {
+                CallButton.Dispose ();
+                CallButton = null;
+            }
+
+            if (PhoneNumberText != null) {
+                PhoneNumberText.Dispose ();
+                PhoneNumberText = null;
+            }
+
+            if (TranslateButton != null) {
+                TranslateButton.Dispose ();
+                TranslateButton = null;
+            }
+        }
     }
 }
